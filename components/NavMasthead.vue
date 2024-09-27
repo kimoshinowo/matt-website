c8bc1400aae621786a0c0648e9150ed8997587dc
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-09-27 14:35:57 +0100

    Create nav component outline

---
+++ components/NavMasthead.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="nav">
+        <span class="nav__title">Matthew Oshinowo</span>
+
+        <ul class="nav__list">
+            <li class="nav__item"><NuxtLink to="/">Home</NuxtLink></li>
+            <li class="nav__item"><NuxtLink to="/about">About</NuxtLink></li>
+            <li class="nav__item"><NuxtLink to="/skills">Skills</NuxtLink></li>
+            <li class="nav__item"><NuxtLink to="/contact">Contact</NuxtLink></li>
+        </ul>
+    </div>
+</template>
