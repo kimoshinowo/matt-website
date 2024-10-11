f89e1d69bca77bbcb076003c411c2a7ee09688b7
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-10-11 11:24:01 +0100

    Add placeholder content to home and about pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <template>
-    <div class="wrap">
+    <div>
         <NavMasthead />
 
-        <div>
-            <h1>Hello, I'm Matt</h1>
-            <h2>An R&D Scientist</h2>
+        <img src="~/assets/images/temp.jpg" class="header-img">
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="content-wrap content-wrap--offset-top">
+            <div class="content-wrap__box content-wrap--flex">
+                <div>
+                    <h1>Hello, I'm Matt</h1>
+                    <h2>An R&D Scientist</h2>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                
+                <img src="https://placehold.co/400x400">
+            </div>
         </div>
         
     </div>
