f7ea7725c49c28630edeecf98db78d0757420679
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-10-18 11:48:14 +0100

    Add router base to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/matt-website/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
