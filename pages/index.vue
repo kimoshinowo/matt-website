890b3ab4160c983515c5bac44801e2e951154acf
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-10-18 11:19:43 +0100

    Add images to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,7 @@
     <div>
         <NavMasthead />
 
-        <img src="~/assets/images/temp.jpg" class="header-img">
-
-        <div class="content-wrap content-wrap--offset-top">
+        <div class="content-wrap content-wrap--bg content-wrap--flex">
             <div class="content-wrap__box content-wrap--flex">
                 <div>
                     <h1>Hello, I'm Matt</h1>
@@ -13,7 +11,7 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
                 
-                <img src="https://placehold.co/300x300">
+                <img src="~/assets/images/headshot.jpeg" class="image image--centered">
             </div>
         </div>
         
