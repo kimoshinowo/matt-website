5295f78418102fd36af9a0b71904d5c45d6a45bf
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-10-20 09:29:48 +0100

    Add base of contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
         <NavMasthead />
+
+        <div class="content-wrap content-wrap--bg-matt content-wrap--flex">
+            <div class="content-wrap__box content-wrap__box--narrow">
+                <h1>Contact me</h1>
+
+                <p>You can find me on linked in <a href="http://linkedin.com/in/matthewoshinowo/">here</a></p>
+
+                <p>Or fill in this form to email me directly:</p>
+
+                <form>
+
+                </form>
+            </div>
+        </div>
     </div>
 </template>
   
