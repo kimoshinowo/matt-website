2b2daa838b5fec211154bb8d0c2aace8fe55161e
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-10-20 09:29:40 +0100

    Add missing spans and use div where appropriate

--- pages/about.vue
+++ pages/about.vue
@@ -5,11 +5,11 @@
         <div class="colour-block"></div>
 
         <div class="content-wrap content-wrap--grid">
-            <span class="intro">
+            <div class="grey-box">
                 <h1>About me</h1>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </span>
+            </div>
 
             <span></span>
             <h2>Education</h2>
@@ -33,7 +33,7 @@
             <span></span>
             <h2>Career</h2>
 
-            <img src="https://placehold.co/300x300">
+            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
 
             <span>
                 <h3>Lorem ipsum</h3>
@@ -41,7 +41,7 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </span>
 
-            <img src="https://placehold.co/300x300">
+            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
 
             <span>
                 <h3>Lorem ipsum</h3>
