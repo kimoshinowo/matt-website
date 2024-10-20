cbe5f4b00e6a865e85c73ac0dfe4feb4caafdee2
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-10-20 09:57:22 +0100

    Add form to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,15 +3,29 @@
         <NavMasthead />
 
         <div class="content-wrap content-wrap--bg-matt content-wrap--flex">
-            <div class="content-wrap__box content-wrap__box--narrow">
+            <div class="content-wrap__box content-wrap__box--narrow content-wrap--margin-tb">
                 <h1>Contact me</h1>
 
                 <p>You can find me on linked in <a href="http://linkedin.com/in/matthewoshinowo/">here</a></p>
 
-                <p>Or fill in this form to email me directly:</p>
+                <form action="https://api.staticforms.xyz/submit" method="post" class="form">
+                    <p>Or fill in this form to email me directly:</p>                  
 
-                <form>
+                    <input type="hidden" name="accessKey" value="ccfa0f3a-0514-4f55-82b8-65a283206098">
 
+                    <label for="name">Your name</label>
+                    <input type="text" name="name">
+
+                    <label for="email">Your email address</label>
+                    <input type="email" name="email">
+
+                    <label for="subject">Subject</label>
+                    <input type="text" name="subject">
+
+                    <label for="message">Message</label>
+                    <textarea name="message"></textarea>
+                    
+                    <input type="submit" value="Email me" />
                 </form>
             </div>
         </div>
