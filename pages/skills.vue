3926eed46396f1ebb5c1e109a35b904a2ecd8265
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-10-20 09:30:09 +0100

    Add skills page content

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,6 +1,60 @@
 <template>
     <div>
         <NavMasthead />
+
+        <div class="colour-block"></div>
+
+        <div class="content-wrap content-wrap--grid">
+            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+
+            <span>
+                <h3>Electrochemistry</h3>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </span>
+
+            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+
+            <span>
+                <h3>Organic Synthesis</h3>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </span>
+
+            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+
+            <span>
+                <h3>Polymerisation</h3>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </span>
+
+            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+
+            <span>
+                <h3>Polymer Functionalisation</h3>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </span>
+
+            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+
+            <span>
+                <h3>Material Characterisation</h3>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </span>
+
+            <div class="grey-box content-wrap--flex">
+                <span>
+                    <h2>Personal Skills</h2>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </span>
+
+                <img src="~/assets/images/climbing.jpeg" class="image__asset image--300w">
+            </div>
+        </div>
     </div>
 </template>
   
