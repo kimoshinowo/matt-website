292eed470e2fd8c0c525d150213573bd2c83505b
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-10-20 19:53:22 +0100

    Add mobile colour blocks to skills page

--- pages/skills.vue
+++ pages/skills.vue
@@ -4,46 +4,48 @@
 
         <div class="colour-block"></div>
 
-        <div class="content-wrap content-wrap--grid">
-            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+        <div class="content-wrap">
+            <div class="content-wrap--grid">
+                <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
 
-            <span>
-                <h3>Electrochemistry</h3>
+                <span>
+                    <h3 class="colour-block--mobile">Electrochemistry</h3>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </span>
 
-            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+                <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
 
-            <span>
-                <h3>Organic Synthesis</h3>
+                <span>
+                    <h3 class="colour-block--mobile">Organic Synthesis</h3>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </span>
 
-            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+                <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
 
-            <span>
-                <h3>Polymerisation</h3>
+                <span>
+                    <h3 class="colour-block--mobile">Polymerisation</h3>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </span>
 
-            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+                <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
 
-            <span>
-                <h3>Polymer Functionalisation</h3>
+                <span>
+                    <h3 class="colour-block--mobile">Polymer Functionalisation</h3>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </span>
 
-            <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+                <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
 
-            <span>
-                <h3>Material Characterisation</h3>
+                <span>
+                    <h3 class="colour-block--mobile">Material Characterisation</h3>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </span>
+            </div>
         </div>
 
         <div class="content-wrap__box--grey content-wrap--flex">
