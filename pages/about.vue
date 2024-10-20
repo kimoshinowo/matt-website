aaaf98661d0c200baa39f909303f3ac6620d3b87
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-10-20 19:17:17 +0100

    Add new colour blocks for mobile

--- pages/about.vue
+++ pages/about.vue
@@ -15,18 +15,28 @@
                 <span></span>
                 <h2>Education</h2>
 
-                <span class="image--300w image--shadow"><img src="~/assets/images/york.png" alt="University of York logo" class="image__asset"></span>
+                <span class="image--300w image--shadow hide-mobile"><img src="~/assets/images/york.png" alt="University of York logo" class="image__asset"></span>
 
                 <span>
-                    <h3>University of York</h3>
+                    <h3 class="colour-block--mobile content-wrap--flex content-wrap--flex-mobile-row">
+                        University of York
+                        <span class="show-mobile image--icon image--centered">
+                            <img src="~/assets/images/york.png" alt="University of York logo" class="image__asset">
+                        </span>
+                    </h3>
 
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </span>
 
-                <span class="image--300w image--shadow"><img src="~/assets/images/bath.jpg" alt="University of Bath logo" class="image__asset"></span>
+                <span class="image--300w image--shadow hide-mobile"><img src="~/assets/images/bath.jpg" alt="University of Bath logo" class="image__asset"></span>
 
                 <span>
-                    <h3>University of Bath</h3>
+                    <h3 class="colour-block--mobile content-wrap--flex content-wrap--flex-mobile-row">
+                        University of Bath
+                        <span class="show-mobile image--icon image--centered">
+                            <img src="~/assets/images/bath.jpg" alt="University of Bath logo" class="image__asset">
+                        </span>
+                    </h3>
 
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </span>
@@ -34,18 +44,28 @@
                 <span></span>
                 <h2>Career</h2>
 
-                <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+                <span class="image--300w image--shadow hide-mobile"><img src="https://placehold.co/300x300" class="image__asset"></span>
 
                 <span>
-                    <h3>Lorem ipsum</h3>
+                    <h3 class="colour-block--mobile content-wrap--flex content-wrap--flex-mobile-row">
+                        Lorem ipsum
+                        <span class="show-mobile image--icon image--centered">
+                            <img src="https://placehold.co/300x300" alt="" class="image__asset">
+                        </span>
+                    </h3>
 
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </span>
 
-                <span class="image--300w image--shadow"><img src="https://placehold.co/300x300" class="image__asset"></span>
+                <span class="image--300w image--shadow hide-mobile"><img src="https://placehold.co/300x300" class="image__asset"></span>
 
                 <span>
-                    <h3>Lorem ipsum</h3>
+                    <h3 class="colour-block--mobile content-wrap--flex content-wrap--flex-mobile-row">
+                        Lorem ipsum
+                        <span class="show-mobile image--icon image--centered">
+                            <img src="https://placehold.co/300x300" alt="" class="image__asset">
+                        </span>
+                    </h3>
 
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </span>
