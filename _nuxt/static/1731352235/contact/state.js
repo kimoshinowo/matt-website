5772c0291ab56532c9e9a76586fe7dbf33b3302e
mattyoladipo <mattyoladipo@gmail.com>
2024-11-11 19:12:57 +0000

    7eb2bdb

---
+++ _nuxt/static/1731352235/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmatt-website\u002F_nuxt\u002Fstatic\u002F1731352235",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002Fmatt-website\u002F",assetsPath:"\u002Fmatt-website\u002F_nuxt\u002F",cdnURL:a}}}}(null));
